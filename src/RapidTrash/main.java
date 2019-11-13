4cb00c82be3c07de61c42d878f3025ec98a74291
Iejun <19086229@student.hhs.nl>
2019-11-13 15:02:31 +0100

    test

--- src/RapidTrash/main.java
+++ src/RapidTrash/main.java
@@ -2,6 +2,6 @@ package RapidTrash;
 
 public class main {
         public static void main(String[] args) {
-                System.out.print("dit is een test bericht");
+                System.out.print("dit is een test bericht, test test");
         }
 }
